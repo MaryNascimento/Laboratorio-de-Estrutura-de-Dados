3b262cf271a8cb978aa1d8221ba7dd6274499db0
Mariana <mariana.do.nascimento.e@gmail.com>
2023-08-13 11:12:55 -0300

    Exercício 2 - Tribonacci

--- Aulas Práticas/Exercício Prático 2 - Revisão Ponteiros e Alocação Dinâmica/Exercício 2/tribonacci.cpp
+++ Aulas Práticas/Exercício Prático 2 - Revisão Ponteiros e Alocação Dinâmica/Exercício 2/tribonacci.cpp
@@ -2,17 +2,48 @@
 
 using namespace std;
 
-int main(){
-    int* Assinatura = new int[3];
-    int n;
+void tribonacci (int* Sequencia, int n){
+    int ultimo, penult, antipenult;
 
+    n = n+3;
+    Sequencia = new int[n];
+
+    cout<<"Preencha os 3 digitos da Assinatura"<<endl;
     for(int i = 0; i < 3; i++){
-        cout<<"Digite um numero inteiro positivo"<<endl;
-        cin>>Assinatura[i];
+        cin>>Sequencia[i];
     }
 
-    cout<<"Quantos Elementos tera a sequencia?"<<endl;
+    cout<<"===== Assinatura ====="<<endl;
+    for(int i = 0; i < 3; i++){
+        cout<<Sequencia[i]<<" ";
+    }
+
+    cout<<endl;
+
+    cout<<"===== Sequencia de Tribonacci ====="<<endl;
+
+    for (int i = 0; i < n; i++){
+        antipenult = Sequencia[i];
+        penult = Sequencia[i + 1];
+        ultimo = Sequencia[i + 2];
+        Sequencia[i + 3] = antipenult + penult + ultimo;
+
+        cout<<Sequencia[i]<<" ";
+    }
+
+}
+
+int main(){
+    int* Sequencia;
+    unsigned int n;
+
+    cout<<"Quantos Elementos deseja calcular?"<<endl;
     cin>>n;
 
+    if(n == 0){
+        cout<<"[ ]"<<endl;
+    } else {
+        tribonacci(Sequencia, n);
+    }
 
 }
